91654717db75ebf6492f813614884c7271943cf1
bengin34 <becaglar3434@gmail.com>
2023-03-17 10:47:11 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}  />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
