03e8f321146446c8d60d1f1b241226b7a0d758af
bengin34 <becaglar3434@gmail.com>
2023-03-18 10:26:04 +0300

    test2

--- src/Request.js
+++ src/Request.js
@@ -1,15 +1,3 @@
-// const key =process.env.REACT_APP_API_KEY
-
-// const requests = {
-//   requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
-//   requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
-//   requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
-//   requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
-//   requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
-// };
-
-// export default requests;
-
 
 const key = process.env.REACT_APP_API_KEY;
 
@@ -34,7 +22,7 @@ const requests = [
   },
   {
     title: " Horror",
-    url: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    url: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=hor&page=1&include_adult=false`,
   },
 ];
 export default requests;
