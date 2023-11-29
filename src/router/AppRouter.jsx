b6733f165e07743800b50be7320be54715a1d602
bengin34 <118957608+bengin34@users.noreply.github.com>
2023-11-29 16:25:08 +0100

    Update AppRouter.jsx

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -6,7 +6,7 @@ import Home from "../pages/Home";
 import Register from "../pages/Register";
 import Login from "../pages/Login";
 import Account from "../pages/Account";
-
+//
 
 const AppRouter = () => {
   return (
